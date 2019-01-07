44f339ff5c643dda66513d079e027e7964764e95
Rowan Lin <xdwlf32@gmail.com>
2019-01-06 18:39:43 -0800

    add seed file for populating database

--- app.js
+++ app.js
@@ -3,28 +3,14 @@ var   express = require("express"),
       mongoose= require("mongoose"),
       Product= require("./models/product"),
       Review = require("./models/review"),
+      seedDB = require("./seed"),
       app     = express();
 
 mongoose.connect("mongodb://localhost/sample_shop");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
-
-// var newProduct={
-//   name: "Cyborg Eye",
-//   category: "tech",
-//   image: "images/cyborgeye.jpg",
-//   description: "A beautifully designed replacement eye for cyborgs",
-//   inventory: 2
-// }
-//
-// Product.create(newProduct, function(err, newlyCreatedProduct){
-//   if(err){
-//     console.log(err);
-//   } else{
-//     console.log(newlyCreatedProduct);
-//   }
-// })
+seedDB();
 
 
 app.get("/", function(req,res){
@@ -36,10 +22,9 @@ app.get("/products", function(req,res){
     if(err){
       console.log(err);
     } else{
-      res.render("tech",{products: allProducts});
+      res.render("products",{products: allProducts});
     }
   })
-  res.render("products");
 })
 
 app.get("/products/clothing", function(req,res){
---
+++ seed.js
@@ -0,0 +1,61 @@
+var mongoose= require("mongoose"),
+    Product = require("./models/product"),
+    Review = require("./models/review");
+
+var data =[{
+  name: "Cyborg Eye",
+  category: "tech",
+  image: "/images/cyborgeye.jpg",
+  description: "A beautifully designed replacement eye for cyborgs",
+  inventory: 2
+},
+{
+  name: "Black Hole Holder",
+  category: "tech",
+  image: "/images/blackholeholder.jpg",
+  description: "A mechanism for holding up to 5 black holes for easy access.",
+  inventory: 5
+},
+{
+  name: "Bottled Unicorn Dreams",
+  category: "consumables",
+  image: "/images/bottledunicorndreams.jpg",
+  description: "Carefully extracted from northwestern wild unicorns. Sustainable harvested.",
+  inventory: 12
+},
+{
+  name: "Bottomless Sack",
+  category: "clothing",
+  image: "/images/bottomlesssack-sametechnologyassanta.jpg",
+  description: "Gorgeous bottomless sack for storing gadgets. Same technology as used in Santa's workshop",
+  inventory: 3
+},
+{
+  name: "Butt Cup",
+  category: "tech",
+  image: "/images/buttcoolingcupholder.jpg",
+  description: "The coolest holder for your buttocks.",
+  inventory: 5
+}
+]
+
+function seedDB(){
+  Product.remove({},function(err){
+    if(err){
+      console.log(err);
+    } else{
+      console.log("removed all products");
+      data.forEach(function(seed){
+        Product.create(seed, function(err,product){
+          if(err){
+            console.log(err);
+          } else{
+            console.log("added product");
+          }
+        })
+      })
+    }
+  });
+}
+
+module.exports= seedDB;
